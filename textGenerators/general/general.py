c7d822e5628193b1ec1815a9dc4553859e9a0122
Stefan Milbert (Torobert2.0.11) <mail@stefanmilbert.de>
2021-08-17 20:48:39 +0200

    if a message is created now depends on a configurable chance

--- textGenerators/general/general.py
+++ textGenerators/general/general.py
@@ -2,6 +2,7 @@
 
 from torolib import torolib
 import logging
+import random
 
 class textGenerator(object):
 
@@ -9,6 +10,7 @@ class textGenerator(object):
         self.log = logging.getLogger(__name__)
         self.log.info('initializing textGenerator extension ' + __loader__.name)
         self.torolib = torolib(redis = redis)
+        self.config = self.torolib.getJsonContent(fileName=f'/etc/torobert/{__loader__.name}.config.json')
         #self.texts = self.torolib.getJsonContent(f'textGenerators/{__loader__.name}/texts.json')
         self.texts = self.torolib.getTextsFromExcel(f'/etc/torobert/{__loader__.name}.texts.xlsx')
         self.textIndices = self.torolib.getTextIndices(self.texts)
@@ -30,8 +32,12 @@ class textGenerator(object):
 
     def getMessageOnRecurrence(self, variables={}, calledBy=''):
         '''returns a message to be read, is triggered on recurring intervals during normal operations or by the webApp'''
-        logging.info('getText was called')
-        message =  self.torolib.buildMessage(texts = self.texts, textIndices = self.textIndices, tags=['top'], variables=variables)
+        if random.random() < float(self.config.get('chanceOfExecution',0.2)):
+            logging.info('getText was called')
+            message =  self.torolib.buildMessage(texts = self.texts, textIndices = self.textIndices, tags=['top'], variables=variables)
+        else:
+            logging.info('getText was called - but silenced by chance')
+            message = None
         #if message:
             #message['keepAudioFile'] = True # this makes sure that mp3 files are not automatically deleted by the talk module, a good thing for static text
         return message
